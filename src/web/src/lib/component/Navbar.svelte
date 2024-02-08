d2938cde2f24f9b995398b8d556b7f35a8909b30
IPowerW <Leon@wenderoth.de>
2024-02-09 00:44:56 +0100

    adj(navbar): fix big window view

--- src/web/src/lib/component/Navbar.svelte
+++ src/web/src/lib/component/Navbar.svelte
@@ -62,21 +62,21 @@
 		<div
 			class="{navbarOpen == false
 				? 'hidden'
-				: ''} top-16 right-2 z-10 absolute w-1/2 md:block md:w-auto"
+				: ''} md:top-auto top-16 md:right-5 right-2 z-10 md:absolute w-1/2 md:block md:w-auto"
 		>
 			<ul
 				class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
 			>
 				<li>
 					<a
-						href="#"
+						href="home"
 						class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
 						aria-current="page">Home</a
 					>
 				</li>
 				<li>
 					<a
-						href="#"
+						href="about"
 						class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
 						>About</a
 					>
